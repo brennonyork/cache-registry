65150fee5c9cc7a642579049166b7dd9140018db
Brennon York <brennon.york@gmail.com>
2013-11-13 23:08:13 -0500

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cache-registry "1.1.1"
+(defproject cache-registry "1.1.2"
   :description "Cache once, fail on filesystem registry"
   :url "https://github.com/brennonyork/cache-registry"
   :license {:name "Eclipse Public License"
